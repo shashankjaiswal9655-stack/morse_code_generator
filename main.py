2db0e1622506d96e02cd197fd36cbf4935de6d1b
Shashank <shashank.email>
2025-10-06 00:48:03 +0530

    The complete program

---
+++ main.py
@@ -0,0 +1,50 @@
+morse_code_rules = {
+    'a': '·−',
+    'b': '−···',
+    'c': '−·−·',
+    'd': '−··',
+    'e': '·',
+    'f': '··−·',
+    'g': '−−·',
+    'h': '····',
+    'i': '··',
+    'j': '·−−−',
+    'k': '−·−',
+    'l': '·−··',
+    'm': '−−',
+    'n': '−·',
+    'o': '−−−',
+    'p': '·−−·',
+    'q': '−−·−',
+    'r': '·−·',
+    's': '···',
+    't': '−',
+    'u': '··−',
+    'v': '···−',
+    'w': '·−−',
+    'x': '−··−',
+    'y': '−·−−',
+    'z': '−−··',
+    '0': '−−−−−',
+    '1': '·−−−−',
+    '2': '··−−−',
+    '3': '···−−',
+    '4': '····−',
+    '5': '·····',
+    '6': '−····',
+    '7': '−−···',
+    '8': '−−−··',
+    '9': '−−−−·',
+    ' ': '/'
+}
+
+morse_code = ""
+sentence = input('What is your sentence ? : \n')
+sentence_ = [letters for letters in sentence]
+
+morse = [letter if letter not in morse_code_rules else morse_code_rules[letter] for letter in sentence_]
+
+for item in morse :
+    morse_code += f'{item} '
+
+print(f'The morse code for the sentence is : {morse_code}')
